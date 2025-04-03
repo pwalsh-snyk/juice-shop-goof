80eb076e93b61e9d1f3876d25c7ea89d368ef874
Patrick Walsh <patrick.walsh@snyk.io>
2025-04-03 15:04:23 -0500

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+# ECR ReadOnly Access for juice-shop-br node group
 resource "aws_iam_role_policy_attachment" "nodegroup_ecr_readonly" {
   role       = module.eks.eks_managed_node_groups["juice-shop-br"].iam_role_name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
